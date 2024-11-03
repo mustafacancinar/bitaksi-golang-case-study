02c345aabea1e4061b841ba82afd83fe97b74427
Mustafa Çınar <cancinar57@gmail.com>
2024-11-03 15:24:30 +0300

    Implement CreateHandler and add validation logic.

--- driver-location/cmd/app/handlers.go
+++ driver-location/cmd/app/handlers.go
@@ -1,13 +1,41 @@
 package app
 
 import (
+	"encoding/json"
 	"net/http"
+	"github.com/go-playground/validator/v10"
 
 	"github.com/cinarizasyon/bitaksi-golang-case-study/driver-location/internal"
 )
 
 func CreateHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from create handler"))
+	defer r.Body.Close()
+	var request CreateDriverLocationRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	err = validate.Struct(request)
+	if validationErrors, ok := err.(validator.ValidationErrors); ok {
+		errors := FormatValidationErrors(validationErrors)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errors)
+		return
+	}
+
+	service := internal.NewLocationDriverService()
+	err = service.CreateDriverLocation(request.Longitude, request.Latitude)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
 
 func BulkCreateHandler(w http.ResponseWriter, r *http.Request) {
